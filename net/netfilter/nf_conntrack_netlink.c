4f53874b856129d36bf03efb5c2969fb91ac8a6c
Andi Rijal Habibi <isibondt@gmail.com>
2026-05-16 02:54:41 +0700

    Update nf_conntrack_netlink.c

--- net/netfilter/nf_conntrack_netlink.c
+++ net/netfilter/nf_conntrack_netlink.c
@@ -3080,7 +3080,7 @@ nla_put_failure:
 	return -1;
 }
 
-#if defined(CONFIG_NF_CONNTRACK_EVENTS) && !defined(CONFIG_NF_CONNTRACK_CHAIN_EVENTS)
+#ifdef CONFIG_NF_CONNTRACK_EVENTS
 static int
 ctnetlink_expect_event(unsigned int events, const struct nf_exp_event *item)
 {
